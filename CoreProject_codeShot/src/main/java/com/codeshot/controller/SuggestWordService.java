7634c03c0bf68765599371a0cb50216f217f3d7a
smhrd <smhrd@DESKTOP-H3NQ08I>
2022-11-03 09:31:30 +0900

    2022-11-03/김지수/SuggestWordService 파일 수정

--- CoreProject_codeShot/src/main/java/com/codeshot/controller/SuggestWordService.java
+++ CoreProject_codeShot/src/main/java/com/codeshot/controller/SuggestWordService.java
@@ -1,22 +1,16 @@
 package com.codeshot.controller;
 
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class SuggestWordService
- */
-public class SuggestWordService extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+import com.codeshot.command.Command;
 
-	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+public class SuggestWordService implements Command {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) {
 		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
